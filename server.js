df8dc4f01108ce4ad4643bbdd04520acf922dc00
adam-niclasson <adam-niclasson@users.noreply.github.com>
2021-02-12 13:18:37 +0100

    modules
    Mongoose and a module added

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const ejs = require('ejs')
+const databaseModule = require('./module.js')
 const app = express()
 const port = 3000
 
 const clientDir = __dirname + "\\client\\"
+
+app.use(express.static(clientDir))
+app.use(express.json())
+app.use(express.urlencoded())
+app.set('view-engine', 'ejs')
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
